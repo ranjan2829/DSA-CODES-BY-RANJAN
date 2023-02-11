a76dce081eb3babae21684391b2892329bea22ba
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-11 20:22:26 +0530

    done

---
+++ JavaBasics/src/basic.java
@@ -0,0 +1,11 @@
+ public class basic {
+    public static void main(String[] args){
+        System.out.println("ranjan");
+        System.out.println("shitole");
+        System.out.println(args[2]);
+
+
+
+    }
+
+}
