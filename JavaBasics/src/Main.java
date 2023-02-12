db2ec359bf0de5d863fa4a0ff6ce2c47fa550b75
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-12 19:25:42 +0530

    java basics

--- JavaBasics/src/Main.java
+++ JavaBasics/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+        //scanner class input object
         Scanner input =new Scanner(System.in);
         System.out.println(input.nextInt());
 
--- JavaBasics/src/Primitives.java
+++ JavaBasics/src/Primitives.java
@@ -1,2 +1,21 @@
-public class Primitives {
+
+import java.util.Scanner;
+public class Primitives{
+
+    public static void main(String[] args){
+        Scanner input =new Scanner(System.in);
+        int roll= 23;
+        char c ='r';
+        float marks=56.7f;
+        double large =34534345345.345345345;
+        long largeint=4325353453534534L;
+        boolean check =false;
+
+
+
+
+
+
+    }
+
 }
