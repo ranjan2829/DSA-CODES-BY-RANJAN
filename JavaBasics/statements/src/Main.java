c5c52fb611ac3117de373470468490e6a819295b
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-13 20:08:00 +0530

    done

---
+++ JavaBasics/statements/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main (String[] args){
+    Scanner input = new Scanner(System.in);
+    //String fruit = input.next();
+    String a ="Ranjan";
+    String b="Ranjan";
+    System.out.println(a==b);
+
+    }
+}
