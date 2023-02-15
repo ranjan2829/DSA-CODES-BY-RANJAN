3ac8b4d538a30709e07e859d529a39c16f1cd885
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-15 20:14:34 +0530

    linear search

---
+++ Java_Algorithm/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,4,5,6,7,8,9,10};
+        int key =7;
+        int ans=linear(arr,key);
+        System.out.println(ans);
+
+    }
+    static int linear(int[] arr,int key){
+        if(arr.length==0){
+            return -1;
+        }
+        for ( int i =0;i<arr.length;i++){
+            int ele=arr[i];
+            if(ele==key){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+}
