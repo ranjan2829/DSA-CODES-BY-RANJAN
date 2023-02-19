a4376d3c88c1c132426987b19be54625fea5e5d0
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-19 19:39:46 +0530

    ceiling of the number

--- BinarySearch/AgnosticBinarySearch/src/Main.java
+++ BinarySearch/AgnosticBinarySearch/src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1};
+        int key = 4;
+        System.out.println(Agnostic(arr, key));
+    }
+
+    static int Agnostic(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+
+        boolean isAsc = arr[low] < arr[high];
+        while (low <= high) {
+            int mid = low+(high-low)/2;
+            if (key == arr[mid]) {
+                return mid;
+            }
+            if(isAsc){
+                if(key<arr[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+
+
+                }
+
+            }
+            else{
+                if(key>arr[mid]){
+                    high=mid-1;
+
+                }
+                else {
+
+                    high = mid - 1;
+                }
+
+            }
+
+
+        }
+        return -1;
+
     }
 }
---
+++ BinarySearch/CeilingNumber.java
@@ -0,0 +1,2 @@
+public class CeilingNumber {
+}
