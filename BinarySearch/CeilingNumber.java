ab2d896adad5dd055bab078d4a7176c1515a1803
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-22 19:16:40 +0530

    ceiling of a number

--- BinarySearch/CeilingNumber.java
+++ BinarySearch/CeilingNumber.java
@@ -1,6 +1,6 @@
 public class CeilingNumber {
     public static void main(String[] args){
-        int[] arr ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+        int[] arr ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19};
         int target=15;
         int ans=Ceiling(arr,target);
         System.out.println(ans);
@@ -11,12 +11,12 @@ public class CeilingNumber {
             return -1;
         }
         int start=0;
-        int end=arr.length;
+        int end=arr.length-1;
         while(start<=end){
             int mid=start + (end - start) / 2;
             if(target<arr[mid]){
                 end=mid-1;
-
+//end = mid - 1;
             }
             else if (target>arr[mid]){
                 start=mid+1;
@@ -27,7 +27,7 @@ public class CeilingNumber {
             }
 
         }
-        return start;
+        return arr[start];
 
     }
 }
