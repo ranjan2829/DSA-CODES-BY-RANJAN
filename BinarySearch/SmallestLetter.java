055fed663a754f84f8a44e16a30e755add93faba
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-22 23:51:21 +0530

    smallest of a letter

---
+++ BinarySearch/SmallestLetter.java
@@ -0,0 +1,31 @@
+public class SmallestLetter {
+    public static void main(String[] args){
+        char[] str={'r','a','n','j','a','n'};
+        char key='j';
+        char ans=Ceiling(str,key);
+        System.out.println(ans);
+
+    }
+    static int Ceiling(char[] arr,char key){
+        if(key>arr[arr.length-1]){
+            return arr[0];
+
+        }
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(key<arr[mid]){
+                end=mid-1;
+            }
+            else if (key>arr[mid]){
+                start=mid+1;
+
+            }
+            else if(key==arr[mid]) {
+                return arr[mid-1];
+            }
+        }
+        return arr[start];
+    }
+}
