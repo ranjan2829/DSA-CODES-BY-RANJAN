51bf612a143c1568ab203ba10c01b20c59f37f22
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-23 00:05:05 +0530

    smallest of a letter

--- BinarySearch/SmallestLetter.java
+++ BinarySearch/SmallestLetter.java
@@ -6,26 +6,30 @@ public class SmallestLetter {
         System.out.println(ans);
 
     }
-    static int Ceiling(char[] arr,char key){
-        if(key>arr[arr.length-1]){
-            return arr[0];
-
-        }
-        int start=0;
-        int end=arr.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(key<arr[mid]){
-                end=mid-1;
-            }
-            else if (key>arr[mid]){
-                start=mid+1;
+    public char nextGreatestLetter(char[] letters, char target) {
+        static int Ceiling(char[] arr,char key){
+            if(key>arr[arr.length-1]){
+                return arr[0];
 
             }
-            else if(key==arr[mid]) {
-                return arr[mid-1];
+            int start=0;
+            int end=arr.length-1;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(key<arr[mid]){
+                    end=mid-1;
+                }
+                else if (key>arr[mid]){
+                    start=mid+1;
+
+                }
+                else if(key==arr[mid]) {
+                    return arr[mid-1];
+                }
             }
+            return arr[start];
         }
-        return arr[start];
+
     }
+
 }
