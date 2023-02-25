db65d01e4eda764122023250baa0ccc580765d09
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-02-25 12:11:46 +0530

    facebook question

--- BinarySearch/FirstLast_occurence.java
+++ BinarySearch/FirstLast_occurence.java
@@ -1,6 +1,33 @@
 public class FirstLast_occurence {
     public static void main (String[] args){
 
+
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int [] ans={-1,-1};
+
+        int start=0;
+        int end= nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target>nums[mid]){
+                start=mid+1;
+            }
+            else if (target<nums[mid]) {
+                end=mid-1;
+
+            }
+            else {
+                return mid;
+            }
+
+
+
+        }
+        return ans;
+
+
+
     }
 
 
