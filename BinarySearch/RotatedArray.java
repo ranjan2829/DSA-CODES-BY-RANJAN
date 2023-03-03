f05beaedbbac558788d6934c00eb084b664ead60
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-03-03 13:29:39 +0530

    Rotated Array

--- BinarySearch/RotatedArray.java
+++ BinarySearch/RotatedArray.java
@@ -1,2 +1,33 @@
-public class RotatedArray {
+public class RotatedArray{
+    public static void main(String[] args){
+        int[] arr={4,5,6,7,0,1,2};
+        System.out.println(SearchRotatedArray(arr));
+
+    }
+    static int SearchRotatedArray(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid =start+(end-start)/2;
+
+
+            //4cases
+
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid>start && arr[mid]>arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid]<=arr[start]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+
+
+        }
+        return -1;
+    }
 }
