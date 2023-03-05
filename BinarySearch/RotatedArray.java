3ebe3420d695a3aa23947fb6cd30cdb58462166f
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-03-05 20:34:06 +0530

    lol

--- BinarySearch/RotatedArray.java
+++ BinarySearch/RotatedArray.java
@@ -1,11 +1,13 @@
 public class RotatedArray {
     public static void main(String[] args){
+        int[] arr={4,5,6,7,0,1,2};
+        System.out.println(FindPivot(arr));
 
 
     }
-    static int index(int[] arr,int target){
+    //public int Search(int[] arr,int target){
 
-    }
+    //}
     static int FindPivot(int[] arr){
         int start=0;
         int end=arr.length-1;
@@ -13,12 +15,12 @@ public class RotatedArray {
             int mid=start+(end-start)/2;
             //4cases to be checked
             if(mid<end && arr[mid]>arr[mid+1]){
-                return mid
+                return mid;
             }
             if(mid>start && arr[mid]<arr[mid-1]){
                 return mid-1;
             }
-            if(arr[mid]<=arr[start){
+            if(arr[mid]<=arr[start]){
                 end=mid-1;
 
             }
