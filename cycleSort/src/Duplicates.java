3bf4875ca79b9f4c6ed25b7428852dd612abc9d3
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-03-25 22:56:13 +0530

    cycle sort

--- cycleSort/src/Duplicates.java
+++ cycleSort/src/Duplicates.java
@@ -1,33 +1,31 @@
-package cycleSort.src;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class Duplicates {
-    public static void main(String[] args){
+class Solution {
+    public List<Integer> findDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
 
-    }
-    static List<Integer> findDuplicates(int[] arr) {
-        int i=0;
-        while(i<arr.length){
-            if(arr[i]!=i+1){
-                int correct = arr[i]-1;
-                if(arr[correct]!=arr[i]){
-                    swap(arr,i,correct);
+                int correct = arr[i] - 1;
+                if (arr[correct] != arr[i]) {
+                    swap(arr, i, correct);
 
-                }
-                else {
+                } else {
                     i++;
                 }
         }
+
         List<Integer> ans = new ArrayList<>();
-        for(i =0;i<arr.length;i++){
-            if(arr[i]!=){
-                ans.add(i);
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] !=i+1) {
+                ans.add(arr[i]);
             }
         }
         return ans;
 
+
+
+        }
 
     }
     static void swap(int[] arr, int first, int second){
@@ -39,3 +37,4 @@ public class Duplicates {
     }
 
 }
+
