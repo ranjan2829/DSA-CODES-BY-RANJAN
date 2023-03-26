7f138a118da57e3b42618f0c4003806dda47cf0b
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-03-26 13:19:24 +0530

    cyclesort algo

--- cycleSort/src/Solution.java
+++ cycleSort/src/Solution.java
@@ -1,35 +1,31 @@
+package cycleSort.src;
+
+import java.util.Arrays;
 class Solution {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-
-
-
-
-            int i=0;
-            while(i<arr.length){
-                int correct=arr[i]-1;
-                if(arr[correct]!=arr[i]){
-                    swap(arr,i,correct);
-
-
-
-                }
-                else{
-                    i++;
-                }
-        }
-        List<Integer> ans =new ArrayList<>();
-        for(int index=0;index<arr.length;index++){
-            if(arr[index]!=index+1){
-                ans.add(index+1);
+    public int firstMissingPositive(int[] arr) {
+        int i=0;
+        while(i<arr.length){
+            int correct =arr[i];
+            if(arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
             }
         }
-        return ans;
+        for(int index= 0;index<arr.length;index++){
+            if(arr[index]!=index+1){
+                return index+1;
+            }
+        }
+        return arr.length+1;
 
 
     }
-    static void swap(int[] arr,int num1,int num2){
-        int temp=arr[num1];
-        arr[num1]=arr[num2];
-        arr[num2]=temp;
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[second]=arr[first];
+        arr[second]=temp;
     }
+
 }
