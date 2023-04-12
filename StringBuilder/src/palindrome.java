3dea725d67926a0c8bfc89761ac8fdbbf0415691
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-12 21:53:25 +0530

    done

--- StringBuilder/src/palindrome.java
+++ StringBuilder/src/palindrome.java
@@ -2,6 +2,19 @@ package StringBuilder.src;
 
 public class palindrome {
     public static void main(String[] args){
+        String str ="abcdcba";
+        System.out.println(check(str));
 
+    }
+    static boolean check(String str){
+        str=str.toLowerCase();
+        for(int i=0;i<str.length()/2;i++){
+            char start = str.charAt(i);
+            char end =str.charAt(str.length()-1-i);
+            if(start!=end){
+                return false;
+            }
+        }
+        return true;
     }
 }
