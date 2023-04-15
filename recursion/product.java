3af60b8319c7b93542f1c0457cf7c3af9f138b07
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-15 09:52:35 +0530

    done

--- recursion/product.java
+++ recursion/product.java
@@ -1,4 +1,18 @@
 package recursion;
 
 public class product {
+    public static void main(String[] args){
+        product(8);
+
+
+    }
+    static void product(int n){
+        if(n==1){
+            return;
+        }
+        System.out.println(n*n-1);
+        product(n-1);
+
+    }
+
 }
