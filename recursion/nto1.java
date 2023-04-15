2871de1d750c3d9a2e4cf2c61f8995002524abcc
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-15 09:24:36 +0530

    done

--- recursion/nto1.java
+++ recursion/nto1.java
@@ -1,4 +1,20 @@
 package recursion;
 
 public class nto1 {
+    public static void main (String[] args){
+        int num=8;
+        System.out.println(series(num));
+
+    }
+    static int series(int n){
+        System.out.println(n);
+
+        if(n==1){
+            return n;
+        }
+        return series(n-1);
+
+
+
+    }
 }
