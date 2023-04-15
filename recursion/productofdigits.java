347ad41739887308b04189046d7887cedf0f11a0
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-15 22:20:54 +0530

    done

--- recursion/productofdigits.java
+++ recursion/productofdigits.java
@@ -7,7 +7,7 @@ public class productofdigits {
 
     }
     static int product(int n){
-        if(n%10==0){
+        if(n%10==n){
             return n;
         }
         return (n%10)*product(n/10);
