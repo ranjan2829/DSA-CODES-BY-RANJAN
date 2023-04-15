b6c193e8998b6033ac898a34fda8d19d00b50874
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-15 23:43:06 +0530

    done

--- recursion/revsersenumber.java
+++ recursion/revsersenumber.java
@@ -1,10 +1,19 @@
 package recursion;
 
-public class revsersenumber {
+public class revsersenumber{
     public static void main(String[] args){
 
     }
     static int reverse(int n){
+
+
+
+
+
+
+
+
+
 
     }
 }
