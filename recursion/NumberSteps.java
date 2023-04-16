b12f44aee9a0b6c866e83e728f721c790527ee39
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-16 18:12:27 +0530

    done

--- recursion/NumberSteps.java
+++ recursion/NumberSteps.java
@@ -2,18 +2,9 @@ package recursion;
 //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 
 public class NumberSteps {
-    public static void main(String[] args){
-        System.out.println(steps(14));
+    public static void main(String[] args) {
 
     }
-    static int counter =0;
-    private static int steps(int n){
-        if(n%2!=0){
-            steps(n-1);
-        }
-        int ans=n/2;
-        counter=counter+1;
-        steps(ans);
-        return counter;
-    }
+
 }
+
