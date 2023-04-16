2665340b60c7d7346b4b774ba33c869733c74325
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-16 18:49:01 +0530

    done

--- recursion/NumberSteps.java
+++ recursion/NumberSteps.java
@@ -3,7 +3,20 @@ package recursion;
 
 public class NumberSteps {
     public static void main(String[] args) {
+        //System.out.println(NumSteps);
 
+    }
+     public static int Numsteps(int n){
+        return helper(n,0);
+    }
+    public int helper(int n,int c){
+        if(n==0){
+            return c;
+        }
+        if(n%2==0){
+            return helper(n/2,c+1);
+        }
+        return helper(n-1,c+1);
     }
 
 
