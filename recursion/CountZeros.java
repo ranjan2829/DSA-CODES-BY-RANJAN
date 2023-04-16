739cb2cd3cfd3fe48b70ca599b08a97ac51c6b5e
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-16 17:59:43 +0530

    done

--- recursion/CountZeros.java
+++ recursion/CountZeros.java
@@ -2,7 +2,26 @@ package recursion;
 
 public class CountZeros {
     public static void main(String[] args){
+        int num=01110030;
+        count(num);
+        System.out.println(count(num));
 
+
+    }
+    static int count(int n){
+        return helper(n,0);
+
+
+    }
+    private static int helper(int n,int c){
+        if(n==0){
+            return c;
+        }
+        int rem=n%10;
+        if(rem==0){
+            return helper(n/10,c+1);
+        }
+        return helper(n/10,c);
     }
 
 }
