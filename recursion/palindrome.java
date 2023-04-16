8091c9991ba943e197fba39e1b8dee6912e41c4a
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-16 15:52:43 +0530

    done

--- recursion/palindrome.java
+++ recursion/palindrome.java
@@ -2,28 +2,30 @@ package recursion;
 
 public class palindrome {
     public static void main(String[] args){
+        check(12345);
+
 
 
 
     }
     static int sum=0;
-    static void reverse(int n){
+    static int reverse(int n){
         if(n==0){
-            return;
+            return sum;
         }
         int rem=n%10;
         sum=sum*10+rem;
         reverse(n/10);
     }
     static int check(int n){
-        int ans= reverse(12345);
+        int ans= reverse(n);
         if(ans==n){
             return 1;
         }
         else{
             return 0;
         }
-        return -1;
+
     }
 
 }
