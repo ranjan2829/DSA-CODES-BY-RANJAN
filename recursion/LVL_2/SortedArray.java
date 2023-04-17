27cd8b54fa2322a600d0a1ffa076b81697ddc2d6
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-17 22:15:58 +0530

    done

--- recursion/LVL_2/SortedArray.java
+++ recursion/LVL_2/SortedArray.java
@@ -1,4 +1,20 @@
 package recursion.LVL_2;
 
 public class SortedArray {
+    public static void main(String[] args){
+
+    }
+    static void SortArray(int[] arr){
+        return helper(arr,0);
+    }
+    private int helper(int[] arr, int c){
+        if(arr.length==0){
+            return -1;
+        }
+        if(arr[c]>arr[c+1]){
+
+
+        }
+
+    }
 }
