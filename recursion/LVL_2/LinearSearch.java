80b2ba5ec0e74e452e491139ad8cda54eb043b8c
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-17 23:11:02 +0530

    done

--- recursion/LVL_2/LinearSearch.java
+++ recursion/LVL_2/LinearSearch.java
@@ -2,6 +2,17 @@ package recursion.LVL_2;
 
 public class LinearSearch {
     public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7,8,9};
+        int target=8;
+        System.out.println(search(arr,0,target));
+
+    }
+    static boolean search(int[] arr,int c,int target){
+        if(arr[c]==target){
+            return true;
+        }
+        return arr[c]!=target && search(arr,c+1,target);
+
 
     }
 
