c638303241df44a16171f17622334086fe92e8fa
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-21 22:31:56 +0530

    done

--- recursion/LVL_2/LinearSearchMultiple.java
+++ recursion/LVL_2/LinearSearchMultiple.java
@@ -1,15 +1,11 @@
 package recursion.LVL_2;
-
 import java.util.ArrayList;
-
 public class LinearSearchMultiple {
     public static void main(String[] args){
-
         int[] arr={2,3,4,5,6,7,8,9,1};
         int target=1;
 
         System.out.println(search(arr,0,target));
-
 
         }
         static ArrayList<Integer> list = new ArrayList<>();
