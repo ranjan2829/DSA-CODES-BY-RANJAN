9a6554de792ce78499ddd7781fe61ff80e9bae71
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-24 15:14:33 +0530

    done

--- recursion/LVL_2/ReturnArrayList.java
+++ recursion/LVL_2/ReturnArrayList.java
@@ -1,7 +1,22 @@
 package recursion.LVL_2;
 
+import java.util.ArrayList;
+
 public class ReturnArrayList {
     public static void main(String[] args){
+        int[] arr={1,2,4,5,6,7,8,8,8,9,10};
+
+        System.out.println(Findall(arr,8,0, ArrayList<Integer> list);
+
+    }
+    static ArrayList<Integer> Findall(int[] arr,int target,int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        return Findall(arr, target, index + 1, list);
 
     }
 }
