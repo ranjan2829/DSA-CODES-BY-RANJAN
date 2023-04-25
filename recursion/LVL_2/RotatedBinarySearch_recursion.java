00804d66769abe62a57e55d31f1bafd35e7570ef
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-25 11:43:21 +0530

    done

--- recursion/LVL_2/RotatedBinarySearch_recursion.java
+++ recursion/LVL_2/RotatedBinarySearch_recursion.java
@@ -4,4 +4,22 @@ public class RotatedBinarySearch_recursion {
     public static void main(String[] args){
 
     }
+    static int search (int[] arr,int target,int s,int e){
+        if(s>e){
+            return -1;
+
+        }
+        int m =s+(e-s)/2;
+        if(arr[m]==target){
+            return m;
+        }
+        if(arr[s]<=arr[m]){
+            if(target>=arr[s]&&target<=arr[m]){
+                return search(arr,target,s,m-1);
+            }
+            else{
+                return search(arr,target,m+1,e);
+            }
+        }
+    }
 }
