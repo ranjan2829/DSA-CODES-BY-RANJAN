d707002cbb142a2bbee1b1c1216e095558bf42d6
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-29 05:25:04 +0530

    done

--- recursion/LVL_2/Triangle.java
+++ recursion/LVL_2/Triangle.java
@@ -1,4 +1,19 @@
 package recursion.LVL_2;
 
 public class Triangle {
+    public static void main(String[] args){
+        int ans= 5;
+        System.out.println(Pattern(ans));
+
+    }
+    static int Pattern(int i){
+        if(i==0){
+            return 1;
+        }
+        char c= '*';
+        System.out.println(c*i);
+        Pattern(i-1);
+        return 0;
+
+    }
 }
