807c44e4441c8fb0290e7a51896c966951b6e45f
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-29 14:07:18 +0530

    done

--- recursion/LVL_2/BubbleSort.java
+++ recursion/LVL_2/BubbleSort.java
@@ -2,9 +2,25 @@ package recursion.LVL_2;
 
 public class BubbleSort {
     public static void main(String[] args){
+        int arr={8,7,6,5,4,3,2,1};
+        System.out.println(BubbleSort(arr,0,1));
 
     }
-    static void BubbleSort(){
+    static void BubbleSort(int[] arr,int i,int j){
+        if(arr.length==0){
+            return;
+        }
+        if(arr[i]>arr[j]){
+            swap(arr,i,j);
+        }
+        else{
+            return BubbleSort(arr,i+1,j+i+1);
+        }
 
+    }
+    static void swap(int[] arr,int a , int b){
+        int temp = arr[a];
+        arr[b]=arr[a];
+        arr[a]=temp;
     }
 }
