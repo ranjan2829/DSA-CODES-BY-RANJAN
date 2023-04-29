099622638d0575d732e1d47d6836793abfc95c1e
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-29 21:53:23 +0530

    done

--- recursion/LVL_3/SelectionSort.java
+++ recursion/LVL_3/SelectionSort.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
 public class SelectionSort {
+    public static void main(String[] args){
+        int[] arr= {8,6,5,4,3,2,1};
+        SelectionSort(arr,arr.length,0,0);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void SelectionSort(int[] arr,int r,int c, int max){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            if(arr[c]>arr[max]){
+                SelectionSort(arr,r,c+1,c);
+            }
+            else{
+                SelectionSort(arr,r,c+1,max);
+            }
+        }
+        else{
+            int temp =arr[max];
+            arr[max]=arr[r-1];
+            arr[r-1]=temp;
+            SelectionSort(arr,r-1,0,0);
+        }
+    }
 }
