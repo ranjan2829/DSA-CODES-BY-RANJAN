3b2389a17951fa0e265063f20a4ffa9a244abd82
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-04-30 23:49:01 +0530

    done

--- MergeSort/src/Merge.java
+++ MergeSort/src/Merge.java
@@ -1,11 +1,30 @@
+import java.util.Arrays;
+
 public class Merge {
 
     public static void main(String[] args){
 
     }
-    static void MergeSort(int[] arr){
+    static int[] MergeSort(int[] arr) {
+
+
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = MergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = MergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+        return merge(left, right);
 
     }
+    private static int[] merge(int[] first,int[] second){
+
+    }
+
+
+
+
 
 
 
