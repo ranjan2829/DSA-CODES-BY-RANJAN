726276e72170d33ed6fab160bce29fe2e8035a84
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-04 09:28:11 +0530

    done

--- QuickSort/src/Main.java
+++ QuickSort/src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
-//        sort(arr, 0, arr.length - 1);
-//        System.out.println(Arrays.toString(arr));
-        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
 
     }
 
