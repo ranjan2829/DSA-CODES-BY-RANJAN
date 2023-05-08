08125a567f44418660c18493edc8d1dfebc6eddb
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-08 22:50:44 +0530

    done

--- QuickSort/src/Main.java
+++ QuickSort/src/Main.java
@@ -40,8 +40,8 @@ public class Main {
                 e--;
             }
         }
-        quicksort(arr,low,e);
-        quicksort(arr,s,high);
+        //quicksort(arr,low,e);
+       // quicksort(arr,s,high);
     }
 
 
