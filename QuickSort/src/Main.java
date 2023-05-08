eaf5cbc7a9c985335f1fd450e56abce12751cf27
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-08 22:39:55 +0530

    done

--- QuickSort/src/Main.java
+++ QuickSort/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr ={8,7,6,5,4,3,2,1};
+        quicksort(arr,arr.length-1,0);
+        System.out.println(Arrays.toString(arr));
 
 
 
