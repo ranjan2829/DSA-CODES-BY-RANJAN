eef2bfb2afcda2c97d5957c43a9bde9982186033
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-13 12:59:51 +0530

    done

--- SubSequence/src/Main.java
+++ SubSequence/src/Main.java
@@ -1,8 +1,10 @@
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         Subsequence("","abc");
+        Sub("","abc");
 
 
 
@@ -16,6 +18,19 @@ public class Main {
         char ch = up.charAt(0);
         Subsequence(p+ch,up.substring(1));
         Subsequence(p,up.substring(1));
+
+    }
+    static ArrayList<String> Sub(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> left= Sub(p+ch,up.substring(1));
+        ArrayList<String> right =Sub(p,up.substring(1));
+        left.addAll(right);
+        return left;
 
     }
 }
