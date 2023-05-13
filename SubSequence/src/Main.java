b809481c53a3b226f6db17a568d2ec41f4dace57
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-13 12:49:10 +0530

    done

--- SubSequence/src/Main.java
+++ SubSequence/src/Main.java
@@ -1,5 +1,21 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Subsequence("","abc");
+
+
+
+
+    }
+    static void Subsequence(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        Subsequence(p+ch,up.substring(1));
+        Subsequence(p,up.substring(1));
+
     }
 }
