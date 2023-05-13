6dd4ee0f31681452d7ad79eaa79117488edc5d3b
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-13 23:08:32 +0530

    done

--- SubSequence/src/Asccii.java
+++ SubSequence/src/Asccii.java
@@ -1,9 +1,20 @@
 public class Asccii {
     public static void main(String[] args){
+        Aci("","abc");
 
 
     }
-    static void Aci(){
+    static void Aci(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+
+        }
+        char ch=up.charAt(0);
+        Aci(p+ch,up.substring(1));
+        Aci(p,up.substring(1));
+        Aci(p+(ch+0),up.substring(1));
+
 
     }
 }
