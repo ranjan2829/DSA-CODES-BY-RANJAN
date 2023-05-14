64fda19d4008ad678d819097e1b91731ad595962
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-14 13:37:31 +0530

    done

--- Permutation-Recursion/src/Main.java
+++ Permutation-Recursion/src/Main.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        permutation("","abc");
+        System.out.println(permutations("","abc"));
 
     }
     static void permutation(String p, String up){
@@ -15,6 +17,22 @@ public class Main {
             permutation(f+ch+s,up.substring(1));
 
         }
+    }
+    static ArrayList<String> permutations(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list =new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch= up.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i=0;i<=p.length();i++){
+            String f=p.substring(0,i);
+            String s=p.substring(i,p.length());
+            ans.addAll(permutations(f+ch+s,up.substring(1)));
+
+        }
+        return ans;
     }
 
 }
