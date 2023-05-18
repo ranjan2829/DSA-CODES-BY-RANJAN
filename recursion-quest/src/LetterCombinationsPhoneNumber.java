4bbe1d7957cbb8e5fc3b7025d078906816136f5e
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-18 17:45:42 +0530

    codes

--- recursion-quest/src/LetterCombinationsPhoneNumber.java
+++ recursion-quest/src/LetterCombinationsPhoneNumber.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCombinationsPhoneNumber {
     public static void main(String[] args){
         combo("","abc");
-        Subsequence("","abc");
+
 
     }
     static void combo(String p,String up) {
@@ -15,6 +16,21 @@ public class LetterCombinationsPhoneNumber {
         combo(p + ch, up.substring(1));
         combo(p, up.substring(1));
     }
+    static List<String> letter(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> left = letter(p+ch,up.substring(1));
+        ArrayList<String> right = letter(p,up.substring(1));
+        left.addAll(right);
+        return left;
+
+    }
+
+
 
 
 }
