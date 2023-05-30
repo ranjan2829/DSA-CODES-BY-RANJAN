e0a28eb7439797457e86827b5fba18d590f6b4f3
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-30 21:40:10 +0530

    codes

--- recursion-quest/src/Dice.java
+++ recursion-quest/src/Dice.java
@@ -1,6 +1,17 @@
 public class Dice {
     public static void main(String[] args){
+        Sub("",4);
 
+    }
+    static void Sub(String p,int target){
+        if(target==0){
+            System.out.println(p);
+            return;
+        }
+        for(int i=1;i<=6 && i<=target;i++){
+            Sub(p+i,target-i);
+
+        }
     }
 
 }
