1168b64bcffd3f2d26c28d2f983bcb1e6e01c6bf
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-30 21:56:29 +0530

    codes

--- recursion-quest/src/Dice.java
+++ recursion-quest/src/Dice.java
@@ -1,6 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Dice {
     public static void main(String[] args){
         Sub("",4);
+        System.out.println(Subarray("",4));
+
 
     }
     static void Sub(String p,int target){
@@ -12,6 +17,19 @@ public class Dice {
             Sub(p+i,target-i);
 
         }
+    }
+    static ArrayList<String> Subarray(String p, int target){
+        if(target==0){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=1;i<=6 && i<=target;i++){
+            list.addAll(Subarray(p+i,target-i));
+
+
+        }return list;
     }
 
 }
