442246a40b8203fd67b71596c7502be928158d7c
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-06-10 10:56:33 +0530

    done

--- Leetcode-Geeks4Geeks/src/missing_number.java
+++ Leetcode-Geeks4Geeks/src/missing_number.java
@@ -1,16 +1,29 @@
+import java.util.Arrays;
 public class missing_number {
     public static void main(String[] args){
+        int[] array={9,8,7,6,4,3,2,1,0};
+        int n= array.length;
+        System.out.println(missing(array,n));
 
     }
 
 
-    int missingNumber(int array[], int n) {
-        for(int i=0;i<array.length;i++){
-            if(i!=array[i]){
-                return i;
+
+
+    public static int missing(int[] arr,int n){
+        int i=0;
+        int x=0;
+
+
+        while(i<arr.length);
+            if(arr[i]==i){
+                i++;
+            }
+            else {
+                x=i;
             }
         }
-
+        return x;
 
     }
 
