120a57064c715826b7afa11708d6232c3937bfbb
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-06-10 10:07:58 +0530

    done

--- Leetcode-Geeks4Geeks/src/Solution.java
+++ Leetcode-Geeks4Geeks/src/Solution.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 public class Solution
 {
@@ -6,26 +7,23 @@ public class Solution
     public static void main(String[] args ){
         int[] A={4,5,6,7,8,9,1};
         int N=A.length;
-        System.out.println(findSum(A,N));
+        findSum(A,N);
+        System.out.println(Arrays.toString(A));
 
     }
-    public static int findSum(int A[],int N)
+    static void findSum(int A[],int N)
     {
-        int x=0,y=0;
-
-        for(int i=0;i<N-1;i++){
-            if(A[i]<A[i+1]){
-                x=A[i+1];
+        for(int i=0;i<A.length;i++){
+            for(int j=1;j< A.length-1;j++){
+                if(A[j]<A[j-1]){
+                    int temp=A[j];
+                    A[j]=A[j-1];
+                    A[j-1]=temp;
+                }
             }
-            else {
-                y=A[i];
-            }
-
         }
-        int u=x+y;
-        System.out.println(x);
-        System.out.println(y);
-        return u;
+
+
 
 
         
