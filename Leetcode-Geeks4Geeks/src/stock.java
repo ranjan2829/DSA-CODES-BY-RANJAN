c6d4a4db906ced5d0a68e20f2775c237ba5e33ac
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-06-10 23:28:06 +0530

    done

--- Leetcode-Geeks4Geeks/src/stock.java
+++ Leetcode-Geeks4Geeks/src/stock.java
@@ -1,28 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
-public class stock {
-    public static void main(String[] args){
-        int[] arr = {100,180,260,310,40,535,695};
-
-        System.out.println(stockBuySell(arr,7));
-
-
-
-
-    }
-    static ArrayList<Integer> stockBuySell(int[] arr, int n) {
-        ArrayList<Integer> list=new ArrayList<>();
-        ArrayList<Integer> main=new ArrayList<>();
-        for(int i=0;i< arr.length-1;i++){
-            if(arr[i]<arr[i+1]){
-                list.add(i);
-                list.add(i+1);
-
-
-            }
-            main.addAll(list);
+for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[i - 1]) {
+        ArrayList<Integer> transaction = new ArrayList<>();
+        transaction.add(i - 1);
+        transaction.add(i);
+        main.add(transaction);
         }
+        }
+
         return main;
-    }
-}
+        }
