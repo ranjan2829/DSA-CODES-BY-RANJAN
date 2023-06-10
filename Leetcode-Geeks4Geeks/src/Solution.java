26bbc1fc51f214315480f8c376bf482db3215ed1
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-06-10 10:30:39 +0530

    done

--- Leetcode-Geeks4Geeks/src/Solution.java
+++ Leetcode-Geeks4Geeks/src/Solution.java
@@ -14,24 +14,22 @@ public class Solution
     }
     public static int findSum(int A[],int N)
     {
-        for(int i=0;i<A.length;i++){
-            for(int j=1;j< A.length;j++){
-                if(A[j]<A[j-1]){
-                    int temp=A[j];
-                    A[j]=A[j-1];
-                    A[j-1]=temp;
-                }
-            }
-        }
+        Arrays.sort(A);
+
+        //for(int i=0;i<A.length;i++){
+          //  for(int j=1;j< A.length;j++){
+           //     if(A[j]<A[j-1]){
+            //        int temp=A[j];
+             //       A[j]=A[j-1];
+                   // A[j-1]=temp;
+             //   }
+          //  }
+        //}
         int min=A[0];
         int max=A[A.length-1];
         int sum=min+max;
         return sum;
 
 
-
-
-        
-        
     }
 }
---
+++ Leetcode-Geeks4Geeks/src/missing_number.java
@@ -0,0 +1,20 @@
+public class missing_number {
+    public static void main(String[] args){
+
+    }
+
+
+    int missingNumber(int array[], int n) {
+        for(int i=0;i<array.length;i++){
+            if(i!=array[i]){
+                return i;
+            }
+        }
+
+
+    }
+
+
+}
+
+        // Your Code Here
