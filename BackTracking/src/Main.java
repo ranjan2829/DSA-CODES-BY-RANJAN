0464625d9762d580d272503edbc3ec66de66c008
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-07-18 14:46:41 +0530

    done

--- BackTracking/src/Main.java
+++ BackTracking/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(count(3,3));
+        path(3,3);
 
     }
     static int count(int r,int c){
@@ -10,5 +11,17 @@ public class Main {
         int left=count(r-1,c);
         int right=count(r,c-1);
         return left+right;
+    }
+    static void path(String p,int r,int c){
+        if(r==1||c==1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            path(p+'D',r-1,c);
+        }
+        if(c>1){
+            path(p+'R',r,c-1);
+        }
     }
 }
