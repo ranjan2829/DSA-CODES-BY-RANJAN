b9e9e128fdcc3f9003f2bf6d0f932ba1ea759b40
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-07-18 18:07:06 +0530

    done

--- BackTracking/src/Main.java
+++ BackTracking/src/Main.java
@@ -4,43 +4,67 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println(count(3,3));
         //path("",3,3);
-        System.out.println(patht("",3,3));
+        System.out.println(patht("", 3, 3));
 
     }
-    static int count(int r,int c){
-        if(r==1||c==1){
+
+    static int count(int r, int c) {
+        if (r == 1 || c == 1) {
             return 1;
         }
-        int left=count(r-1,c);
-        int right=count(r,c-1);
-        return left+right;
+        int left = count(r - 1, c);
+        int right = count(r, c - 1);
+        return left + right;
     }
-    static void path(String p,int r,int c){
-        if(r==1&&c==1){
+
+    static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
             System.out.println(p);
             return;
         }
-        if(r>1){
-            path(p+'D',r-1,c);
+        if (r > 1) {
+            path(p + 'D', r - 1, c);
         }
-        if(c>1){
-            path(p+'R',r,c-1);
+        if (c > 1) {
+            path(p + 'R', r, c - 1);
         }
     }
-    static ArrayList<String> patht(String p, int r, int c){
-        if(r==1&&c==1){
-            ArrayList<String> list=new ArrayList<>();
+
+    static ArrayList<String> patht(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
             list.add(p);
 
             return list;
         }
-        ArrayList<String> list=new ArrayList<>();
-        if(r>1){
-            list.addAll(patht(p+'D',r-1,c));
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1) {
+            list.addAll(patht(p + 'D', r - 1, c));
         }
-        if(c>1){
-           list.addAll(patht(p+'R',r,c-1));
+        if (c > 1) {
+            list.addAll(patht(p + 'R', r, c - 1));
         }
         return list;
     }
+
+    static ArrayList<String> pathd(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1&& c>1) {
+            list.addAll(pathd(p + 'G', r - 1, c-1));
+        }
+        if (r > 1) {
+            list.addAll(pathd(p + 'D', r - 1, c));
+        }
+        if (c > 1) {
+            list.addAll(pathd(p + 'R', r, c - 1));
+        }
+        return list;
+    }
+
 }
