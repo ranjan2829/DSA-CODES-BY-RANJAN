18fdb19f88986d2d46431579ceccc45f87fc8d99
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-07-19 21:42:59 +0530

    done

--- BackTracking/src/NQueens.java
+++ BackTracking/src/NQueens.java
@@ -1,2 +1,27 @@
 public class NQueens {
+    public static void main(String[] args) {
+
+    }
+    static int queens(boolean[][] board,int row){
+        if(row==board.length){
+            display(board);
+            return 1;
+        }
+    }
+    private static void display(boolean[][] board){
+        for(boolean[] row:board){
+            for(boolean element : row){
+                if(element){
+                    System.out.println("Q");
+                }
+                else {
+
+
+                    System.out.println("X");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
