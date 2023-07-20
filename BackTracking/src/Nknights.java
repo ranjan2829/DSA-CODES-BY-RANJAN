c3e2d6e1ad47126ee546cb0148c774968d8a7ae8
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-07-20 23:36:05 +0530

    done

--- BackTracking/src/Nknights.java
+++ BackTracking/src/Nknights.java
@@ -6,17 +6,34 @@ public class Nknights {
         if(knights==0){
             display(board);
         }
+        if(row==board.length-1 && col== board.length){
+            return;
+        }
+        if(col== board.length){
+            knights(board,row+1,0,knights);
+            return;
+        }
+        if(isSafe(board,row,col)){
+            board[row][col]=true;
+            knights(board,row,col+1,knights-);
+            board[row][col]=false;
+
+        }
+        knights(board,row,col+1,knights);
     }
     private static void display(boolean[][] board) {
         for(boolean[] row : board) {
             for(boolean element : row) {
                 if (element) {
-                    System.out.print("Q ");
+                    System.out.print("K ");
                 } else {
                     System.out.print("X ");
                 }
             }
             System.out.println();
         }
+    }
+    private static boolean isSafe(boolean[][] board,int row,int col){
+
     }
 }
