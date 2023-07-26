c5cecfcfc8cf546b4e09ad7539431aaee40ae40a
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-07-26 22:38:53 +0530

    done

---
+++ Leetcode-Geeks4Geeks/GAnagrams.java
@@ -0,0 +1,25 @@
+
+
+public class GAnagrams {
+    public static void main(String[] args) {
+
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        ArrayList<String> list=new ArrayList<>();
+        List<<List<String>> ans = new ArrayList<>();
+
+        if(strs.length==0){
+            list.add("");
+            ans.add(list);
+            return ans;
+        }
+        for(int i=0;i<strs.length;i++){
+
+        }
+
+
+
+
+
+    }
+}
