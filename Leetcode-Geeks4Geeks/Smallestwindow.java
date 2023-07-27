305b5033969edfb12498b2235fdd28dfeb92cca5
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-07-27 16:02:45 +0530

    done

--- Leetcode-Geeks4Geeks/Smallestwindow.java
+++ Leetcode-Geeks4Geeks/Smallestwindow.java
@@ -1,2 +1,21 @@
 public class Smallestwindow {
+    public static void main(String[] args) {
+
+    }
+    static String smallest(String s,String p){
+        int x= s.length();
+        int y=p.length();
+        if(y>x){
+            return "-1";
+
+        }
+        StringBuilder str= new StringBuilder();
+        int max=Integer.MAX_VALUE;
+        int[] freq=new int[26];
+        for(int i=0;i<y;i++){
+            freq[p.charAt(i)-'a']++;
+        }
+    }
+
 }
+
