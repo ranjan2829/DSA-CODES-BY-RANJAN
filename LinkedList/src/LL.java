625657e66c0eaddc16b572baa209ea372ddcb933
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-03 20:35:42 +0530

    done

---
+++ LinkedList/src/LL.java
@@ -0,0 +1,13 @@
+public class LL {
+    private class Node{
+        private int value;
+        private Node next;
+        public Node(int value){
+            this.value=value;
+        }
+        public Node(int vlaue, Node next){
+            this.value=value;
+            this.next=next;
+        }
+    }
+}
