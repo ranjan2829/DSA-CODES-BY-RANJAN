364ac6da314f7576e2931894d6695b6a8d5904d0
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-03 23:34:02 +0530

    done

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         LL list = new LL();
         list.insertfirst(14);
+        list.insertfirst(1);
+        list.insertfirst(10);
+        list.insertfirst(19);
+        list.insertfirst(78);
+        list.insertfirst(56);
+        list.insertfirst(66);
+        list.display();
 
 
 
