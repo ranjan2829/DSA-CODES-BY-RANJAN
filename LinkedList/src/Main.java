822ddcdc886048e388f6813a4e28fb0b36f8d0f7
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-07 21:39:09 +0530

    done

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -8,9 +8,12 @@ public class Main {
         list.insertfirst(78);
         list.insertfirst(56);
         list.insertfirst(66);
-        list.insertlast(69);
-        list.insertmiddle(999,3);
-        System.out.println(list.deletefirst());
+       // list.insertlast(69);
+        //list.insertmiddle(999,3);
+        list.deleteLast();
+
+        System.out.println(list.deleteLast());
+        //System.out.println(list.deletefirst());
         list.display();
 
 
