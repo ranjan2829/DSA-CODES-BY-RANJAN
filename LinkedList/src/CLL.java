2009b83db13a56b3076d8aa1a650c28bfdec9399
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-09 21:29:10 +0530

    done

--- LinkedList/src/CLL.java
+++ LinkedList/src/CLL.java
@@ -5,6 +5,18 @@ public class CLL {
         this.head=null;
         this.tail=null;
     }
+    public void insert(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            head = node;
+            tail = node;
+            return;
+        }
+
+        tail.next = node;
+        node.next = head;
+        tail = node;
+    }
     private class Node {
         int val;
         Node next;
