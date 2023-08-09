81c14af615d2818f5608718c5454fa5c58224029
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-09 21:32:51 +0530

    done

--- LinkedList/src/CLL.java
+++ LinkedList/src/CLL.java
@@ -17,6 +17,34 @@ public class CLL {
         node.next = head;
         tail = node;
     }
+    public void delete(int val) {
+        Node node = head;
+        if (node == null) {
+            return;
+        }
+
+        if (head == tail){
+            head = null;
+            tail = null;
+            return;
+        }
+
+        if (node.val == val) {
+            head = head.next;
+            tail.next = head;
+            return;
+        }
+
+        do {
+            Node n = node.next;
+            if (n.val == val) {
+                node.next = n.next;
+                break;
+            }
+            node = node.next;
+        } while (node != head);
+
+    }
     public void display() {
         Node node = head;
         if (head != null) {
