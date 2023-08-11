ab38769d5222f0ba412175135f1afdf16a330d37
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-11 14:41:02 +0530

    done

--- LinkedList/src/LL.java
+++ LinkedList/src/LL.java
@@ -89,6 +89,22 @@ public class LL {
         }
         return node;
     }
+    public static LL merge(LL first , LL second){
+        Node f= first.head;
+        Node s=second.head;
+        LL ans =new LL();
+        while(f!=null && s!=null){
+            if(f.value<s.value){
+                ans.insertlast(f.value);
+                f=f.next;
+            }
+            else{
+                ans.insertlast(s.value);
+                s=s.next;
+            }
+        }
+
+    }
     public void inserRec(int val, int index){
         head=inserRec(val,index,head);
     }
