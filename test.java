d661d7aa3ea928070ad0fa82af2d5105761a7d18
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-22 21:10:12 +0530

    done

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+
+
+    }
+}
