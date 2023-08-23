2b8cc4f6c3fb9f6cf2015c5750c6a04aee26b1d2
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-23 23:13:39 +0530

    dome

---
+++ stacks-queues/src/custom.java
@@ -0,0 +1,30 @@
+public class custom {
+    private static int[] data;
+    private static final int DEFAULT_SIZE=10;
+    int ptr=-1;
+    public CustomStk(){
+        this(DEFAULT_SIZE);
+    }
+    public CustomStk(int size){
+        this.data=new int[size];
+
+    }
+    public boolean push(int item){
+        ptr++;
+        data[ptr]=item;
+        return true;
+    }
+    public int pop() throws Exception{
+        if(isEmpty()){
+            throw new Exception("nope");
+        }
+
+    }
+    private boolean isFull(){
+        return ptr== data.length-1;
+    }
+    private boolean isEmpty(){
+        return ptr==-1;
+    }
+
+}
