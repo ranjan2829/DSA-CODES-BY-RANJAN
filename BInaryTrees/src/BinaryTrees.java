f931745bd86c4ef88d53dbb8189b70c31ee9678e
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-08-28 13:49:41 +0530

    dome

---
+++ BInaryTrees/src/BinaryTrees.java
@@ -0,0 +1,13 @@
+public class BinaryTrees {
+    public BinaryTrees(){
+
+    }
+    private static class Node(){
+        private int data;
+        Node left;
+        Node right;
+        public Node(int val){
+            this.data=val;
+        }
+    }
+}
