109283e40ddc0002436ec97ee0897851a2b8decd
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-17 12:07:39 +0530

    done

---
+++ AVLTREES/src/AVLTREES.java
@@ -0,0 +1,86 @@
+class AVLTREES {
+        public class Node{
+            private int data;
+            private int height;
+            private Node left;
+            private Node right;
+
+            public Node(int val){
+                this.data=val;
+
+            }
+            public int getValue(){
+                return data;
+
+            }
+            private Node root;
+
+
+
+        }
+        public AVLTREES(){
+
+        }
+        private Node root;
+        public int height(Node node){
+            if(node==null){
+                return -1;
+            }
+            return node.height;
+
+        }
+        public boolean isEmpty(){
+            return root==null;
+        }
+        public void display(Node node , String details){
+            if(node==null){
+                return;
+
+            }
+            System.out.println(details+node.data);
+
+            display(node.left,"Left child of "+node.getValue()+":");
+            display(node.right,"right child of "+node.getValue()+":");
+        }
+        private Node insert(int val,Node node){
+            if(node==null){
+                node=new Node(val);
+                return node;
+            }
+            if(val< node.data){
+                node.left=insert(val,node.left);
+            }
+            if(val>node.data){
+                node.right=insert(val,node.right);
+            }
+            node.height=Math.max(height(node.left),height(node.right))+1;
+            return node;
+
+        }
+        public boolean balacned(){
+            return balanced(root);
+        }
+        private boolean balanced(Node node){
+            if(node==null){
+                return true;
+            }
+            return Math.abs(height(node.left)-height(node.right))<=1 &&balanced(node.left)&&balanced(node.right);
+        }
+        public void preorder(){
+            preorder(root);
+        }
+        private void preorder(Node node){
+            if(node==null){
+                return;
+            }
+            System.out.println(node.data+ " ");
+            preorder(node.left);
+            preorder(node.right);
+
+        }
+
+
+
+}
+
+
