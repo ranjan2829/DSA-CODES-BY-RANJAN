5cbb92eb96856686a5f719fc3b5e87502028afea
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-17 18:55:32 +0530

    done

---
+++ Solution.java
@@ -0,0 +1,28 @@
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result= new ArrayList<>();
+        if(root==null){
+            return null;
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode current=queue.poll();
+
+            }
+        }
+
+
+
+        
+    }
+}
