526e8e4c93f3620953ebb94e3a054b5aba32055c
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-18 23:29:28 +0530

    done

---
+++ BinaryTreeAlgo/src/Solution.java
@@ -0,0 +1,19 @@
+import com.sun.source.tree.Tree;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+
+        
+        
+    }
+}
