fe3b19bbfb5c15590a8369b170d920f22fd18069
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-19 23:30:35 +0530

    code done

---
+++ BinaryTreeAlgo/src/NODELL.java
@@ -0,0 +1,29 @@
+import javax.swing.tree.TreeNode;
+
+public class NODELL {
+    class Solution {
+        public Node connect(Node root) {
+            if(root == null){
+                return root;
+            }
+
+            Node leftmost=root;
+            while(leftmost.left!=null){
+                Node current =leftmost;
+                while(current!=null){
+                    current.left.next=current.right;
+                    if(current.next!=null){
+                        current.right.next=current.next.left;
+                    }
+                    current=current.next;
+
+                }
+                leftmost=leftmost.left;
+
+            }
+            return root;
+
+
+        }
+    }
+}
