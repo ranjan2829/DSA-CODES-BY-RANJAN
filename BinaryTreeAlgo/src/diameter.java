ee226446da45e824f730ccee68051ee98ce47891
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-20 19:47:53 +0530

    code done

--- BinaryTreeAlgo/src/diameter.java
+++ BinaryTreeAlgo/src/diameter.java
@@ -1,5 +1,19 @@
+import javax.swing.tree.TreeNode;
+
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        height(root);
+        return diameter(root);
+
+    }
+    public int height(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+
 
     }
 }
