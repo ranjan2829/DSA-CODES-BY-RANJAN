d1b04d573d37613e667396b36be133b7da586e1d
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-21 18:48:01 +0530

    code done

--- BinaryTreeAlgo/src/stkdfs.java
+++ BinaryTreeAlgo/src/stkdfs.java
@@ -1,2 +1,20 @@
 public class stkdfs {
+    void dfsStack(Node node) {
+        if(node == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+
+        while(!stack.isEmpty()) {
+            Node removed = stack.pop();
+            System.out.print(removed.val + " ");
+            if(removed.right != null) {
+                stack.push(removed.right);
+            }
+            if(removed.left != null) {
+                stack.push(removed.left);
+            }
+        }
+    }
 }
