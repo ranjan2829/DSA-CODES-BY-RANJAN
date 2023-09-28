b9e585faaf7ffa02fdb0236e102b40e391149a21
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-29 00:06:15 +0530

    leetcode sol

--- Leetcode-Geeks4Geeks/pre.java
+++ Leetcode-Geeks4Geeks/pre.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 class Solution {
@@ -12,9 +13,20 @@ class Solution {
         nodes.offer(root);
         while(!nodes.isEmpty()){
             int size=nodes.size;
+            Node curr=nodes.get();
+            ans.add(curr.val);
+            if(curr.left!=null){
+                nodes.offer(curr.left);
+            }
+            if(curr.right!=null){
+                nodes.offer(curr.right);
+            }
+
             
 
+
         }
+        return ans;
         
     }
 }
