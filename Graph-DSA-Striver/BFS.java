7bd3845f6a90e749e3bb14ae01f9137526b99d0f
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-30 01:36:59 +0530

    graph dsa

--- Graph-DSA-Striver/BFS.java
+++ Graph-DSA-Striver/BFS.java
@@ -11,6 +11,11 @@ public class BFS {
         ArrayList<Integer> bfs= new ArrayList<>();
         boolean visited[] = new boolean(v);
         Queue<Integer> q= new LinkedList<>();
+        q.add(0);
+        visited[0]=true;
+        while(!q.isEmpty()){
+            
+        }
     }
     
 
