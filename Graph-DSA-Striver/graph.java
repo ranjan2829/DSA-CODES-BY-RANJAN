03404d78b814a0ef4f1e502a5c69e654909a586d
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-30 00:42:07 +0530

    graph dsa

---
+++ Graph-DSA-Striver/graph.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class graph{
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> ans= new ArrayList<ArrayList<Integer>><>();
+        int n=9;
+        in m=9;
+        for(int i=0i<=n;i++){
+            ans.add(ArrayList<Integer>)
+
+        }
+        //edge 1---->3
+        ans.get(1).add(3);
+    }
+}
