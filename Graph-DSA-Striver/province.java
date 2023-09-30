f3310d38b18c8c3f8c1526ee80034cd7ec904a89
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-30 18:46:32 +0530

    graph dsa

--- Graph-DSA-Striver/province.java
+++ Graph-DSA-Striver/province.java
@@ -18,9 +18,23 @@ class Solution {
         }
         for(int i=0;i<V;i++){
             for(int j=0;j<V;j++){
-                if(adj.get(i))
+                if(adj.get(i).get(j)==1 && i!=j){
+                    adjLs.get(i).add(j);
+                    adjLs.get(j).add(i);
+                }
             }
         }
+        int visited[]=new int[V];
+        int cnt=0;
+        for(int i=0;i<V;i++){
+            if(visited[i]==0){
+                cnt++;
+                dfs(i,adjLs,visited);
+            }
+
+        }
+        return cnt;
+
 
     }
 };
