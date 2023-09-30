1e6ed9ec8fac274560012cc247e00e3d53d89689
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-30 16:40:43 +0530

    graph dsa

--- Graph-DSA-Striver/DFS.java
+++ Graph-DSA-Striver/DFS.java
@@ -2,7 +2,21 @@ import java.util.ArrayList;
 
 class Solution {
     // Function to return a list containing the DFS traversal of the graph.
+    public static void dfs(int node, boolean visited[],ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ls){
+        visited[node]=true;
+        ls.add(node);
+        for(Integer it:adj.get(node)){
+            if(visited[it]==false){
+                dfs(it,visited,adj,ls);
+            }
+        }
+    }
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
+        boolean visited[]=new boolean[V+1];
+        visited[0]=true;
+        ArrayList<Integer> ls=new ArrayList<>();
+        dfs(0,visited,adj,ls);
+        return ls;
     }
 }
