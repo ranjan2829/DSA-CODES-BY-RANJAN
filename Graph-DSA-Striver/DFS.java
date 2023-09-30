0e403b2b0258d5caffc707bc667a2e937bdea8a7
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-30 16:49:55 +0530

    graph dsa

--- Graph-DSA-Striver/DFS.java
+++ Graph-DSA-Striver/DFS.java
@@ -14,7 +14,7 @@ class Solution {
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
         boolean visited[]=new boolean[V+1];
-        visited[0]=true;
+        //visited[0]=true;
         ArrayList<Integer> ls=new ArrayList<>();
         dfs(0,visited,adj,ls);
         return ls;
