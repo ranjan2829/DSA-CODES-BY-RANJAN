d8125f3c2aea7041a969ccd6afc8f2b86d8d130e
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-09-30 16:35:01 +0530

    graph dsa

---
+++ Graph-DSA-Striver/DFS.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+    }
+}
