8bdfdc9f255dc2441dd54432d85c50b3b5f54af9
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-10-08 00:50:54 +0530

    graph dsa

--- Graph-DSA-Striver/alien.java
+++ Graph-DSA-Striver/alien.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 class Solution
+
 {
+    private List<Integer> toposort(int V, List<List<Integer>> adj){
+        int[] indegree=new int[V];
+        for(int i=0;i<V;i++){
+            for(Integer it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.add(indegree[i]);
+                
+            }
+        }
+
+    }
     public String findOrder(String [] dict, int N, int K)
     {
-        // Write your code here
+        
         
         
     }
