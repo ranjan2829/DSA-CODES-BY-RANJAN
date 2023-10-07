84dc74f830952e3643030ce7232de20f63715d11
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-10-07 13:07:14 +0530

    graph dsa

--- Graph-DSA-Striver/course.java
+++ Graph-DSA-Striver/course.java
@@ -44,3 +44,37 @@ class Solution
 
     }
 }
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+
+        }
+        for(int i=0;i<numCourses;i++){
+            adj.get(prerequisites.get(i).get(0)).add(prerequisites.get(i).get(1);
+        }
+        int[] indegree=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            for(int it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.add(indegree[i]);
+            }
+        }
+        int[] topo=new int[numCourses];
+        int idx=0;
+        while(!q.isEmpty()){
+            int node=q.peek();
+            q.remove();
+            
+        }
+
+        
+    }
+}
