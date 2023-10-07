420d5e01af37337d8810d25f4b92206bf0947312
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-10-07 10:54:35 +0530

    graph dsa

--- Graph-DSA-Striver/course.java
+++ Graph-DSA-Striver/course.java
@@ -1,28 +1,7 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-class Solution {
-    public boolean isPossible(int N,int P, int[][] prerequisites)
+class Solution
+{
+    static int[] findOrder(int n, int m, ArrayList<ArrayList<Integer>> prerequisites) 
     {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<N;i++){
-            adj.add(new ArrayList<>());
-        }
-        int n=prerequisites.length;
-        for(int i=0;i<n;i++){
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
-        }
-        int[] indgree=new int[N];
-        for(int i=0;i<N;i++){
-            for(int it:adj.get(i)){
-                indgree[i]++;
-            }
-        }
-        Queue<Integer> q=new LinkedList<>();
-        
-
-
-        
+        // add your code here
     }
-    
 }
