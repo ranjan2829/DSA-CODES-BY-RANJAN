edc6a69ce5f5d4ba337d5e04a7e204ea7eeb198c
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-10-07 11:22:56 +0530

    graph dsa

--- Graph-DSA-Striver/course.java
+++ Graph-DSA-Striver/course.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution
 {
@@ -11,5 +13,34 @@ class Solution
         for(int i=0;i<m;i++){
             adj.get(prerequisites.get(i).get(0)).add(prerequisites.get(i).get(0));
         }
+        int[] indegree=new int[n];
+        for(int i=0;i<n;i++){
+            for(int it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(indegree[i]);
+            }
+        }
+        int[] topo=mew int[n];
+        int ind=0;
+        while(!q.isEmpty()){
+            int node=q.peek();
+            q.remove();
+            topo[ind++]=node;
+            for(int i:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(indegree[it]);
+                }
+            }
+        }
+        if(ind==n) return topo;
+        int[] arr={};
+        return arr;
+
     }
 }
