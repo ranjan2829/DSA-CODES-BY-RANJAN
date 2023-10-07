43a77e5b90312a78b1e6afc0019942ee2fd5de3b
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-10-08 02:08:12 +0530

    graph dsa

--- Graph-DSA-Striver/alien.java
+++ Graph-DSA-Striver/alien.java
@@ -34,10 +34,36 @@ class Solution
             }
 
         }
+        return topo;
 
     }
     public String findOrder(String [] dict, int N, int K)
     {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<K;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<N;i++){
+            String s1=dict[i];
+            String s2=dict[i+1];
+            int len=Math.min(s1.length(),s2.length());
+            for(int ptr=0;ptr<len;ptr++){
+                if(s1.charAt(ptr)!=s2.charAt(ptr)){
+                    adj.get(s1.charAt(ptr)-'a').add(s2.charAt(ptr)-'a');
+                    break;
+                }
+            }
+            List<Integer> ans=toposort(K, adj);
+            String result="";
+
+            for(int it:ans){
+                result=result+(char)(it+(int)('a'));
+
+            }
+            return result;
+            
+
+        }
         
         
         
