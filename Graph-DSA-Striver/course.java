ddef2da4423e766884b76510ff1d82e4b33f1b17
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-10-07 11:07:30 +0530

    graph dsa

--- Graph-DSA-Striver/course.java
+++ Graph-DSA-Striver/course.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+
 class Solution
 {
     static int[] findOrder(int n, int m, ArrayList<ArrayList<Integer>> prerequisites) 
     {
-        // add your code here
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<m;i++){
+            adj.get(prerequisites.get(i).get(0)).add(prerequisites.get(i).get(0));
+        }
     }
 }
