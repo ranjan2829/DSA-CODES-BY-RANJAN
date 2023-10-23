f43eaedc4770c1c0619392ebdf3b6aa13aaa09ec
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-10-24 00:29:10 +0530

    graph dsa

---
+++ Graph-DSA-Striver/djikstras.java
@@ -0,0 +1,9 @@
+class Solution
+{
+    //Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    {
+        // Write your code here
+    }
+}
