b0e9632a40b2a8f99eb0e8dcd3681913e4539138
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-10-25 21:45:22 +0530

    graph dsa

--- Graph-DSA-Striver/djikstras.java
+++ Graph-DSA-Striver/djikstras.java
@@ -1,4 +1,4 @@
-
+import java.util.PriorityQueue;
 
 class Solution
 {
@@ -6,7 +6,28 @@ class Solution
     //from the source vertex S.
     static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
     {//min-heap
-        PriorityQueue<Pair> pq=new PriorityQueue<Pair>((x,y)->x.distance,y.distance);
+        PriorityQueue<Pair> pq=new PriorityQueue<Pair>((x,y)->x.distance-y.distance);
+        int []dist=new int[V];
+        for(int i=0;i<V;i++){
+            dist[i]=(int)(le9);
+
+        }
+        dist[S]=0;;
+        pq.add(new Pair(0, S));
+        while(pq.size()!=0){
+            int dis=pq.peek().distance;
+            int node=pq.peek().node;
+            pq.remove();
+            for(int i=0;i<adj.get(node).size();i++){
+                int edge=adj.get(node).get(i).get(1);
+                int adjnode=adj.get(node).get(i).get(0);
+                if(dis+edge<dist(adjnode)){
+                    dist[adjnode]=dis+edge;
+                    pq.add(new Pair(dis[adjnode], adjnode));
+                }
+            }
+        }
+        return dist;
 
         
     }
