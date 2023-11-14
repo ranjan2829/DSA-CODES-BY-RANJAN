ffd5152797dcdc54685b03ef0426ad680ab7dfc4
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-11-14 19:11:20 +0530

    done

--- Graph-DSA-Striver/mst.java
+++ Graph-DSA-Striver/mst.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+
+class Pair{
+    int node;
+    int distance;
+    public Pair(int distance, int node){
+        this.distance=distance;
+        this.node=node;
+    }
+}
+class triology{
+    int u;
+    int v;
+    int w;
+    triology(int u,int v, int w){
+        this.u=u;
+        this.v=v;
+        this.w=w;
+    }
+}
 class Solution{
 	static int spanningTree(int V, int E, int edges[][]){
-	    // Code Here. 
+        List<List<Pair>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<Pair>());
+        }
+        for(int i=0;i<edges.length;i++){
+            int  u=edges[i][0];
+            int v=edges[i][1];
+            int w=edges[i][2];
+            adj.get(u).add(new Pair(v, w));
+            adj.get(v).add(new Pair(u, w));
+
+        }
+
+
+	    
 	}
 }
